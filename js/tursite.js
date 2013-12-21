91d6a0d9188519bcbf4c88e499731595d35fc4ca
yanazov <yan_azov@mail.ru>
2013-12-21 13:11:04 +0200

    no message

--- js/tursite.js
+++ js/tursite.js
@@ -2,11 +2,35 @@ var current_slide = 1;
 var next_slide = 1;
 var opacity_current = 1;
 var opacity_next = 0;
+var arrow = 0;
 	
 function js_main_slide(arrow){
 	
 	if(arrow == "prev"){
+		next_slide = current_slide - 1;
+		if(next_slide < 1)
+			next_slide = 4;
+		opacity_current -= 0.03;
+		if(opacity_current < 0.5){
+			opacity_current = 0; 
+			opacity_next += 0.02;
+		}
+			document.getElementById("slide"+current_slide).style.opacity = opacity_current;		
+		document.getElementById("slide"+next_slide).style.opacity = opacity_next;		
 		
+	/*document.getElementById("slide"+main_slide).style.opacity = 1;*/
+		if(opacity_next > 1){
+			current_slide -= 1;
+			opacity_current = 1;
+			opacity_next = 0;
+			if(current_slide < 1){
+				current_slide = 4;
+			}
+			return 0;
+		}
+		if(opacity_next < 1){
+			var slider_timer= setTimeout(function() {js_main_slide(arrow)});
+		}
 	
 	}
 	if(arrow == "next"){	
@@ -34,9 +58,7 @@ function js_main_slide(arrow){
 		if(opacity_next < 1){
 			var slider_timer= setTimeout(function() {js_main_slide(arrow)});
 		}
-		
 	}
-	
 }
 
 
